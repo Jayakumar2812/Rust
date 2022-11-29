369f312c7535220650acc2b378c762dacb5b0012
Jayakumar2812 <jayakumargowtham2812@gmail.com>
2022-11-29 17:48:47 +0530

    added 219_contains_duplicate_2

---
+++ easy_prob/src/bin/219_contains_duplicate_2.rs
@@ -0,0 +1,17 @@
+use std::collections::HashMap;
+impl Solution {
+    pub fn contains_nearby_duplicate(nums: Vec<i32>, k: i32) -> bool {
+     
+        let mut lookup:HashMap<i32,i32> = HashMap::new();
+
+        for  (i,v) in nums.iter().enumerate() {
+            let returned_val = lookup.insert(*v,i as i32);
+            if returned_val != None {
+                if (returned_val.unwrap() - i as i32).abs() <= k {
+                    return true
+                }            
+            }
+        }
+        return false
+    }   
+}
